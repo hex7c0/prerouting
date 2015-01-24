96ea5dbfc6b0d9103dd46886acf306f08b62cdbf
hex7c0 <carniellifrancesco@gmail.com>
2015-01-24 19:31:19 +0100

    test chain with compression

--- test/chaining.js
+++ test/chaining.js
@@ -18,11 +18,12 @@ var prerouting = require('..');
 var assert = require('assert');
 var http = require('http');
 var request = require('superagent');
+var snappy = require('snappy');
 
 /*
  * test module
  */
-describe('chaining http', function() {
+describe('normal chaining http request', function() {
 
   var ports = [ 3001, 5001, 5002, 5003, 5004 ];
 
@@ -121,6 +122,87 @@ describe('chaining http', function() {
     it('should return web request to 4° Prerouting server', function(done) {
 
       request.get(uri + ports[4] + '/').end(function(err, res) {
+
+        if (err) {
+          throw err;
+        }
+        assert.equal(res.statusCode, 200);
+        assert.equal(res.text, 'Hello World\n');
+        assert.equal(res.header['x-field'], 'ciao');
+        done();
+      });
+    });
+  });
+});
+
+describe('compressed chaining http request', function() {
+
+  var ports = [ 3011, 5011, 5012 ];
+  var compress = function(input, next) {
+
+    snappy.compress(input, function(err, compressed) {
+
+      if (err) throw err;
+      return next(compressed);
+    })
+  }
+  var uncompress = function(input, next) {
+
+    snappy.uncompress(input, {
+      asBuffer: true
+    }, function(err, uncompress) {
+
+      if (err) throw err;
+      return next(uncompress);
+    })
+  }
+
+  describe('create', function() {
+
+    it('should create Web server', function(done) {
+
+      http.createServer(function(req, res) {
+
+        res.setHeader('X-Field', 'ciao');
+        if (req.url == '/') {
+          res.writeHead(200, {
+            'Content-Type': 'text/plain'
+          });
+        } else {
+          res.writeHead(404, {
+            'Content-Type': 'text/plain'
+          });
+        }
+        res.end('Hello World\n');
+      }).listen(ports[0], done);
+    });
+    it('should create 1° Prerouting server', function(done) {
+
+      prerouting.createServer({
+        toPort: ports[0],
+        listenPort: ports[1],
+        dataToNext: uncompress,
+        dataFromNext: compress
+      }).on('listening', done);
+    });
+    it('should create 2° Prerouting server', function(done) {
+
+      prerouting.createServer({
+        toPort: ports[1],
+        listenPort: ports[2],
+        dataToNext: compress,
+        dataFromNext: uncompress
+      }).on('listening', done);
+    });
+  });
+
+  describe('routing', function() {
+
+    var uri = 'http://127.0.0.1:';
+
+    it('should return web request to 2° Prerouting server', function(done) {
+
+      request.get(uri + ports[2] + '/').end(function(err, res) {
 
         if (err) {
           throw err;
